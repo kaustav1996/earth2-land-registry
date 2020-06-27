a9dd2eaca76f52bf9a07cc9d050227c4c80e8ccb
Kaustav Banerjee <kaustav@banerjee.life>
2020-06-27 10:37:50 +0530

    Updates

---
+++ precache-manifest.4831d485a04c33b0f728ccdc1bc1d83e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddee9c867747d9669cf1",
+    "url": "/earth2-land-registry/static/js/runtime~main.ddee9c86.js"
+  },
+  {
+    "revision": "248f17367e905ceb2b99",
+    "url": "/earth2-land-registry/static/js/main.248f1736.chunk.js"
+  },
+  {
+    "revision": "b7c100ad55b66bb1657f",
+    "url": "/earth2-land-registry/static/js/1.b7c100ad.chunk.js"
+  },
+  {
+    "revision": "248f17367e905ceb2b99",
+    "url": "/earth2-land-registry/static/css/main.a79227a6.chunk.css"
+  },
+  {
+    "revision": "b7c100ad55b66bb1657f",
+    "url": "/earth2-land-registry/static/css/1.5949fddf.chunk.css"
+  },
+  {
+    "revision": "8320c26af188ead668fcb66c5b049e94",
+    "url": "/earth2-land-registry/index.html"
+  }
+];
